9df8097ac33f422ba373b984d95dc52d8eb68399
unknown <jr_web@163.com>
2020-03-15 11:27:11 +0800

    '更新首页轮播图，更新推荐栏'

--- README.md
+++ README.md
@@ -5,10 +5,13 @@
 > css-基础样式；
   img-常用图片；
 ### common 公共的js文件 公共常量-工具类
+> navbar(navigationbar) 导航栏
 ### components 组件
 > common-公共组件，各个项目都可通用的；
   content-当前项目业务相关；
 ### network 网络相关
+> request.js 请求核心处理层 
+  home.js 首页数据请求 <-> home.js <-> request.js 
 ### router 路由
 ### store Vuex状态管理工具
 ### views 主要视图 首页-分类-购物车之类的
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+
+  import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+  import {getHomeMultidata} from 'network/home'
+
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data() {
+      return {
+        // result: null  Test
+        banners: [],
+        recommends: []
+      }
+    },
+    created() { //创建时请求数据
+      //1.请求多个数据
+      getHomeMultidata().then(res => {
+        // this.result = res;//获取信息 Test
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
